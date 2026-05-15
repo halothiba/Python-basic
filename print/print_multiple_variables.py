242ac842309009f876e57d82668c90e7cd205299
J.Thibakar <thiba2006077@gmail.com>
2026-05-15 11:45:29 +0530

    Create print_multiple_variables.py

---
+++ print/print_multiple_variables.py
@@ -0,0 +1,11 @@
+id = 1001
+name = "Thibakar"
+age = 20
+print("My id is:", id)
+print("My name is:", name)
+print("My age is:", age)
+
+print("My id is:" + str(id))
+print("My name is:" + name)
+print("My age is:" + str(age))
+print("result"+str(True))
