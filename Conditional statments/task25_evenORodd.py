89da6577f19a9d32bf90e552d23b00c498370142
J.Thibakar <thiba2006077@gmail.com>
2026-05-26 11:14:27 +0530

    even or odd

---
+++ Conditional statments/task25_evenORodd.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+if num>=0:
+    if num % 2 == 0:
+        print("This number is even number")
+    else:
+        print("This number is odd number")
+else :
+    print("Invalid number! Please enter a positive number")
+    
+    
+    
+#using nested if
