cbbc2326a2b28ac95ea0219fc13761fd6cca5a43
J.Thibakar <thiba2006077@gmail.com>
2026-05-26 11:17:43 +0530

    casting2

---
+++ Type casting/casting2.py
@@ -0,0 +1,24 @@
+# Convert integer to string
+age = 22
+print("My age is " + str(age))
+
+#Convert string to integer
+year ="2026"
+year_i = int(year)
+print("String to Integer:", year_i)
+
+#Convert string to float
+price = "99.99"
+price_f = float(price)
+print("String to float: ", price_f)
+
+#convert integer to float
+quantity = 10
+quantity_f = float(quantity)
+print("Integer to Float: ", quantity_f)
+
+#convert float to Integer
+temprature = 36.7
+temp_i = int(temprature)
+print("Float to Integer: ", temp_i)
+
