d73949bfaaf7d543f6e70bc7a1e63c9f9384b018
J.Thibakar <thiba2006077@gmail.com>
2026-05-26 11:15:10 +0530

    format1

---
+++ Formating/format1.py
@@ -0,0 +1,10 @@
+id = 1025
+name = "Thibakar"
+phone = "0778963576"
+
+print ("My id is ", id)
+print (f"My id is {id} My name is {name}")
+print ("My id is {} My name is {}" .format(id,name))
+print ("My name is {1} My id is {0}" .format(id,name))
+print ("My name is {1} My number is {2} My id is {0}".format(id,name,phone))
+print ("My name is %s My number is %s My id is %d" %(name,phone,id))
