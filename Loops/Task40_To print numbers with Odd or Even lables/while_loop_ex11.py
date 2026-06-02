2b34e5aaccb976d8e772880ec4d8c44364a74a1a
J.Thibakar <thiba2006077@gmail.com>
2026-06-02 10:57:30 +0530

    print numbers with odd or even lables

---
+++ Loops/Task40_To print numbers with Odd or Even lables/while_loop_ex11.py
@@ -0,0 +1,14 @@
+x = 1
+while x <= 10:
+    if x % 2 == 1:
+        print(f"{x} :odd")
+    else:
+        print(f"{x} :even")
+    x += 1
+    
+    
+    #if x % 2 == 1:
+    #        print("x, :odd")
+    #    else:
+    #        print("x, :even")
+    #    x += 1
